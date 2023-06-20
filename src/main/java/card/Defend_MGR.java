03d2132ce352d27f1299179dd9ab306616db322f
tkrym <1960385817@qq.com>
2023-06-20 23:37:55 +0800

    20

--- src/main/java/card/Defend_MGR.java
+++ src/main/java/card/Defend_MGR.java
@@ -4,41 +4,41 @@ import basemod.abstracts.CustomCard;
 import basemod.helpers.BaseModCardTags;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import path.AbstractCardEnum;
 
-public class Strike_MGR extends CustomCard{
-    public static final String ID = "Strike_MGR";
+public class Defend_MGR extends CustomCard{
+    public static final String ID = "Defend_MGR";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String IMG = "img/card/"+ID+".png";
     private static final int COST = 1;
-    private static final int DMG = 6;
-    private static final int PLUS_DMG = 3;
-    public Strike_MGR() {
-        super(ID, cardStrings.NAME, IMG, COST, DESCRIPTION, CardType.ATTACK,
-                AbstractCardEnum.MGR_COLOR, CardRarity.BASIC, CardTarget.ENEMY);
-        this.tags.add(CardTags.STARTER_STRIKE);
-        this.baseDamage = DMG;
+    private static final int BLOCK = 5;
+    private static final int PLUS_BLOCK = 3;
+    public Defend_MGR() {
+        super(ID, cardStrings.NAME, IMG, COST, DESCRIPTION, CardType.SKILL,
+                AbstractCardEnum.MGR_COLOR, CardRarity.BASIC, CardTarget.SELF);
+        this.tags.add(CardTags.STARTER_DEFEND);
+        this.baseBlock = BLOCK;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
     }
 
-    public AbstractCard makeCopy() { return new Strike_MGR(); }
+    public AbstractCard makeCopy() { return new Defend_MGR(); }
 
     public void upgrade() {
         if (!this.upgraded) {
             upgradeName();
-            upgradeDamage(PLUS_DMG);
+            upgradeDamage(PLUS_BLOCK);
         }
     }
 }
---
+++ src/main/java/relic/TheFirst.java
@@ -0,0 +1,35 @@
+package relic;
+import basemod.abstracts.CustomRelic;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import org.apache.commons.lang3.ObjectUtils;
+
+public class TheFirst extends CustomRelic {
+    public static final String ID = "TheFirst";
+    private static final String IMG = "img/relic"+ID+".png";
+    private static final String OUTLINE = "img/relic/outline/"+ID+".png";
+    private static final int DMG=1,BLOCK=1;
+
+    public TheFirst() {
+        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(OUTLINE), RelicTier.STARTER, AbstractRelic.LandingSound.CLINK);
+    }
+
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        this.flash();
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        this.addToBot(new DamageRandomEnemyAction(new DamageInfo(null, DMG, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        this.addToBot(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, BLOCK));
+    }
+
+    public String getUpdatedDescription() { return this.DESCRIPTIONS[0]; }
+
+}
