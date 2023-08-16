eec944b9933eb2fc4cf8b6dd1d4afe9024df10df
tkrym <1960385817@qq.com>
2023-08-16 13:52:54 +0800

    20

--- src/main/java/card/COMMON/MaguroCleave.java
+++ src/main/java/card/COMMON/MaguroCleave.java
@@ -28,7 +28,7 @@ public class MaguroCleave extends AbstractMGRCard {
     private static final int COST = 1;
     private static final int DMG = 6;
     private static final int PLUS_DMG = 3;
-    private static final int MAGIC = 2;
+    private static final int MAGIC = 3;
     private static final int PLUS_MAGIC = 1;
     public MaguroCleave() {
         super(ID, cardStrings.NAME, IMG, COST, DESCRIPTION, CardType.ATTACK,
