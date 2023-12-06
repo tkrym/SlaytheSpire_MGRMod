6b91ab8b8393690e33d633fbe8ef0a6562d02370
tkrym <1960385817@qq.com>
2023-12-06 18:20:17 +0800

    1.1.4

--- src/main/java/card/UNCOMMON/GazeFromTheShadow.java
+++ src/main/java/card/UNCOMMON/GazeFromTheShadow.java
@@ -17,7 +17,7 @@ public class GazeFromTheShadow extends AbstractMGRCard {
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     public static final String IMG = "img/card/"+ID.substring(4)+".png";
     private static final int COST = 1;
-    private static final int MAGIC = 3;
+    private static final int MAGIC = 2;
     private static final int PLUS_MAGIC = 2;
     public GazeFromTheShadow() {
         super(ID, cardStrings.NAME, IMG, COST, DESCRIPTION, CardType.SKILL,
